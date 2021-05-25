f12041b398544182b5462335b48311c46626459c
Aung Soe Kyaw <aungsoekyaw2017@gmail.com>
2021-05-25 21:38:21 +1000

    gui changes update 2

--- sample.py
+++ sample.py
@@ -1,6 +1,6 @@
 #imports
 from tkinter import *
-
+from PIL import ImageTk, Image
 
 #run tkinter
 app = Tk()
@@ -9,11 +9,27 @@ app = Tk()
 app.geometry("500x300")
 app.title("Automatic Item Checkout")
 
+Label1 = Label(app, text = "Welcome to the smart checkout!", font=("Times", "20"))
+Label2 = Label(app, text = "Card Only!", font=("Times", "18", "bold"))
+image = Image.open("guiimg\_Barcode_32896.jpg")
+
+# Reszie the image using resize() method
+resize_image = image.resize((100, 50))
+  
+img = ImageTk.PhotoImage(resize_image)
+  
+# create label and add resize image
+label3 = Label(image=img)
+label3.image = img
+  
 def newWindow():
     app.destroy()
     import sample2
 
-button1 = Button(app, text = "sample2", command=newWindow)
+Label1.pack()
+label3.pack()
+Label2.pack()
+button1 = Button(app, padx= "10", text = "Enter", command=newWindow)
 button1.pack()
 
 app.mainloop()
